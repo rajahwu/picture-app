da2537e9729a00b0f6151e2bb0b1e6357bcfaae2
rajahwu <vmradford@gmail.com>
2023-03-30 19:58:58 -0400

    remove testing console log

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,33 @@ import { useSearchTerm } from "./context/SearchContext";
 import "./App.css";
 
 function App() {
-  const {picType} = useSearchTerm();
-  const [pic, setPic] = useState([])
+  const { picType } = useSearchTerm();
+  const [pic, setPic] = useState([]);
 
   async function getPic(picType) {
     await unsplash.search
       .getPhotos({ query: picType || "cat", page: 1, perPage: 5 })
-      .then((res) => setPic(res.response.results))
-      // .then((err) => console.log("this is err", err));
+      .then((res) => setPic(res.response.results));
+    // .then((err) => console.log("this is err", err));
   }
 
   useEffect(() => {
     getPic(picType);
   }, [picType]);
 
-  useEffect(() => {
-    console.log(picType);
-  }, [picType]);
+  useEffect(() => {}, [picType]);
 
   return (
     <div className="App">
-    <Header title="Cat" />
+      <Header title="Cat" />
       {pic.map((pic) => {
         return (
-          <Image key={pic.id} color={pic.color} urls={pic.urls} alt_description={pic.alt_description} />
+          <Image
+            key={pic.id}
+            color={pic.color}
+            urls={pic.urls}
+            alt_description={pic.alt_description}
+          />
         );
       })}
     </div>
